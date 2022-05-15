1cc643c975cee911d9f6bc63f31d3b57512b5483
jxncdhc <haochengding@ufl.edu>
2022-05-15 15:22:14 -0400

    add pheno data

--- R/HTRF.R
+++ R/HTRF.R
@@ -1,16 +1,16 @@
 #' Short-Term Time-Restricted Feeding Alters Rhythmicity of Lipid and Amino Acid Metabolites in Overweight Men (Human time-restricted feeding data)
-#' 
+#'
 #' @title Human time-restricted feeding data
 #'
-#' @usage 
+#' @usage
 #' HTRF
 #'
 #' @format A data frame with 13167 rows and 125 variables. Each row is gene expression value. Each column represents one sample.
-#' 
-#' 
-#' @references 
-#' Lundell LS, Parr EB, Devlin BL, Ingerslev LR et al. Time-restricted feeding alters lipid and amino acid metabolite rhythmicity without perturbing clock gene expression. Nat Commun 2020 Sep 16;11(1):4643. PMID: 32938935
-#' 
-#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129843}  
-#' 
+#'
+#'
+#' @references
+#' Lundell LS, Parr EB, Devlin BL, Ingerslev LR et al. Time-restricted feeding alters lipid and amino acid metabolite rhythmicity without perturbing clock gene expression. Nat Commun 2020 Sep 16;11(1):4643. PMID: 32938935.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129843}
+#'
 "HTRF"
---
+++ R/pheno.R
@@ -0,0 +1,16 @@
+#' Subjects information for Short-Term Time-Restricted Feeding Alters Rhythmicity of Lipid and Amino Acid Metabolites in Overweight Men (Human time-restricted feeding data)
+#' 
+#' @title Subjects information for Human time-restricted feeding data
+#'
+#' @usage 
+#' pheno
+#'
+#' @format A data frame with 69 rows and 4 columns. Each row represents one subject. First column is subject ID. Second row is phase. Third row is treatment group. Fourth row is time.  
+#' 
+#' 
+#' @references 
+#' Lundell LS, Parr EB, Devlin BL, Ingerslev LR et al. Time-restricted feeding alters lipid and amino acid metabolite rhythmicity without perturbing clock gene expression. Nat Commun 2020 Sep 16;11(1):4643. PMID: 32938935.
+#' 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129843} 
+#' 
+"pheno"

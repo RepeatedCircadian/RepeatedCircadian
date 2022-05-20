483caad7eb22b9154ee2eacbb810627a8053d2b5
jxncdhc <haochengding@ufl.edu>
2022-05-20 15:45:16 -0400

    update HTRF subdata

---
+++ R/subHTRF.R
@@ -0,0 +1,16 @@
+#' Subset of Short-Term Time-Restricted Feeding Alters Rhythmicity of Lipid and Amino Acid Metabolites in Overweight Men Data (Human time-restricted feeding subdata)
+#'
+#' @title Human time-restricted feeding subdata
+#'
+#' @usage
+#' HTRF
+#'
+#' @format A data frame with 1195 rows and 128 variables. Each row is gene expression value. First three columns are three types of gene identification names then each column represents one sample.
+#'
+#'
+#' @references
+#' Lundell LS, Parr EB, Devlin BL, Ingerslev LR et al. Time-restricted feeding alters lipid and amino acid metabolite rhythmicity without perturbing clock gene expression. Nat Commun 2020 Sep 16;11(1):4643. PMID: 32938935.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129843}
+#'
+"subHTRF"
